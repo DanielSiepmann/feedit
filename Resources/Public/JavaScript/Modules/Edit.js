d070c253544b914ae1fb817992cd00d582ab84c4
Daniel Siepmann <coding@daniel-siepmann.de>
2020-08-05 17:11:01 +0200

    WIP|Add check whether feedit will work in frontend
    Due to inner technical workings of TYPO3, we will check whether feedit
    will work at all.
    If not opened from TYPO3 backend, there is no opener context.
    This would prevent a working backend, which is opened by feedit.
    
    Therefore it is checked and all features are disabled with a hint.
    Additionally an explanation is displayed in admin panel.
    
    This is WIP:
    
    Not all buttons need to be disabled. Maybe we can / should add a css
    class as selector to disable only buttons which will open the backend.

--- Resources/Public/JavaScript/Modules/Edit.js
+++ Resources/Public/JavaScript/Modules/Edit.js
@@ -1,59 +1,97 @@
+// Merge context from opener into this window.
+// This allows opened popups to retrieve TYPO3 backend context.
+if (window.opener !== null) {
+    window.TYPO3 = Object.assign({}, window.opener.TYPO3, window.TYPO3);
+}
+
 this.Element && function(ElementPrototype) {
-	ElementPrototype.closest = ElementPrototype.closest ||
-		function(selector) {
-			var el = this;
-			while (el.matches && !el.matches(selector)) el = el.parentNode;
-			return el.matches ? el : null;
-		}
+    ElementPrototype.closest = ElementPrototype.closest ||
+        function(selector) {
+            var el = this;
+            while (el.matches && !el.matches(selector)) el = el.parentNode;
+            return el.matches ? el : null;
+        }
 }(Element.prototype);
 
 function openBackendHandler(event) {
-	event.preventDefault();
-	var element = event.target;
+    event.preventDefault();
+    var element = event.target;
 
-	if (element.tagName !== 'A') {
-		element = element.closest('a.typo3-feedit-btn-openBackend');
-	}
+    if (element.tagName !== 'A') {
+        element = element.closest('a.typo3-feedit-btn-openBackend');
+    }
 
-	var vHWin = window.open(element.getAttribute('data-backendScript'), element.getAttribute('data-t3BeSitenameMd5'));
-	vHWin.focus();
-	return false;
+    var vHWin = window.open(element.getAttribute('data-backendScript'), element.getAttribute('data-t3BeSitenameMd5'));
+    vHWin.focus();
+    return false;
 }
 
 function submitFormHandler(event) {
-	event.preventDefault();
-	var element = event.target;
+    event.preventDefault();
+    var element = event.target;
 
-	if (element.tagName !== 'A') {
-		element = element.closest('a.typo3-feedit-btn-submitForm');
-	}
+    if (element.tagName !== 'A') {
+        element = element.closest('a.typo3-feedit-btn-submitForm');
+    }
 
-	var execute = true;
-	var form = document[element.getAttribute('data-feedit-formname')];
-	var confirmText = element.getAttribute('data-feedit-confirm');
+    var execute = true;
+    var form = document[element.getAttribute('data-feedit-formname')];
+    var confirmText = element.getAttribute('data-feedit-confirm');
 
-	if (confirmText) {
-		execute = confirm(confirmText);
-	}
+    if (confirmText) {
+        execute = confirm(confirmText);
+    }
 
-	if (execute) {
-		form.querySelector('.typo3-feedit-cmd').value = element.getAttribute('data-feedit-cmd');
-		form.submit();
-	}
+    if (execute) {
+        form.querySelector('.typo3-feedit-cmd').value = element.getAttribute('data-feedit-cmd');
+        form.submit();
+    }
 
-	return false;
+    return false;
+}
+
+function disableElement(element) {
+    element.classList.add('disabled');
+    var parentPanel = element.closest('.typo3-editPanel');
+    if (parentPanel !== null) {
+        parentPanel.classList.add('typo3-feedit-not-fully-functional');
+    }
+
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        return false;
+    });
+
+    var buttonTitleElement = element.querySelector('span');
+    if (buttonTitleElement != null) {
+        buttonTitleElement.title = 'Disabled due to … ' + buttonTitleElement.title;
+    }
 }
 
 function initializeEditModule() {
-	var editModuleBtnsOpenBackend = document.querySelectorAll('.typo3-feedit-btn-openBackend');
-	for (var i = 0, len = editModuleBtnsOpenBackend.length; i < len; i++ ) {
-		editModuleBtnsOpenBackend[i].addEventListener('click', openBackendHandler);
-	}
+    var active = true;
+    if (typeof window.TYPO3.settings === "undefined") {
+        var active = false;
+        document.querySelector('.feedit-defect').style.display = 'block';
+    }
 
-	var editModuleBtnsSubmitForm = document.querySelectorAll('.typo3-feedit-btn-submitForm');
-	for (var i = 0, len = editModuleBtnsSubmitForm.length; i < len; i++ ) {
-		editModuleBtnsSubmitForm[i].addEventListener('click', submitFormHandler);
-	}
+    var editModuleBtnsOpenBackend = document.querySelectorAll('.typo3-feedit-btn-openBackend');
+    for (var i = 0, len = editModuleBtnsOpenBackend.length; i < len; i++ ) {
+        if (active) {
+            editModuleBtnsOpenBackend[i].addEventListener('click', openBackendHandler);
+        } else {
+            disableElement(editModuleBtnsOpenBackend[i]);
+        }
+    }
+
+    var editModuleBtnsSubmitForm = document.querySelectorAll('.typo3-feedit-btn-submitForm');
+    for (var i = 0, len = editModuleBtnsSubmitForm.length; i < len; i++ ) {
+        if (active) {
+            editModuleBtnsSubmitForm[i].addEventListener('click', submitFormHandler);
+        } else {
+            disableElement(editModuleBtnsSubmitForm[i]);
+        }
+    }
 }
 
 window.addEventListener('load', initializeEditModule, false);
